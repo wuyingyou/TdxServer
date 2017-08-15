b3ea40c3b8598b7458527d98da9552a30040a3f0
barretwu <barretwu@Objectivasoftware.com>
2017-08-15 13:14:59 +0800

    Add the first version of Tdx server. API test url  - host:port/api/user/2

---
+++ TdxServer/TdxServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TdxServer.Startup))]
+namespace TdxServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ TdxServer/TdxServer/api/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TdxServer.api
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public bool Get(int id)
+        {
+            if (id == 1)
+                return false;
+            return true;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
